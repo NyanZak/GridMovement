47e6f79cdf50b566caf03bb80144b3b417a9cc46
Zak Cronin <71699711+NyanZak@users.noreply.github.com>
2022-03-22 13:40:02 +0000

    Update CameraRotationTrigger.cs

--- Assets/Components/GridMovement/Scripts/CameraRotationTrigger.cs
+++ Assets/Components/GridMovement/Scripts/CameraRotationTrigger.cs
@@ -4,57 +4,22 @@ using UnityEngine;
 
 public class CameraRotationTrigger : MonoBehaviour
 {
-    [SerializeField]
-    GameObject cameraRotator = null;
-
-    [SerializeField]
-    RotationDirection targetDirection = RotationDirection.forward;
-
-    [SerializeField]
-    RotationDirection exitDirection = RotationDirection.forward;
-
+    [SerializeField] GameObject cameraRotator = null;
+    [SerializeField] RotationDirection targetDirection = RotationDirection.forward;
+    [SerializeField] RotationDirection exitDirection = RotationDirection.forward;
     CameraRotation cameraRotation = null;
-
     void Start() {
         cameraRotation = cameraRotator.GetComponent<CameraRotation>();
         GetComponent<MeshRenderer>().enabled = false;
     }
-
     void OnTriggerStay(Collider other) {
         if (other.tag == "Player") {
             cameraRotation.RotateTo(Direction.ToVector(targetDirection));
         }
     }
-
     void OnTriggerExit(Collider other) {
         if (other.tag == "Player") {
             cameraRotation.RotateTo(Direction.ToVector(exitDirection));
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
